c00d8773cdfd8674007c9fe2b1afe6dcc49d85cb
Jimi van der Woning <jimivdwoning@gmail.com>
2013-10-04 11:25:50 +0200

    Improved bond drawing and cleaned code.

--- src/OFraMP.js
+++ src/OFraMP.js
@@ -1,3 +1,8 @@
+var CANVAS_PADDING = 20;
+var ATOM_RADIUS = 10;
+var BOND_SPACING = 4;
+
+
 CanvasRenderingContext2D.prototype.clear = function() {
   this.clearRect(0, 0, this.canvas.width, this.canvas.height);
 }
@@ -23,6 +28,26 @@ Molecule.prototype.init = function(atoms, bonds) {
   this.bonds.init(bonds, this.atoms);
 }
 
+Molecule.prototype.width = function() {
+  return this.atoms.width();
+}
+
+Molecule.prototype.height = function() {
+  return this.atoms.height();
+}
+
+Molecule.prototype.move = function(dx, dy) {
+  return this.atoms.move(dx, dy);
+}
+
+Molecule.prototype.scale = function(f) {
+  return this.atoms.scale(f);
+}
+
+Molecule.prototype.bestFit = function(w, h) {
+  return this.atoms.bestFit(w, h);
+}
+
 Molecule.prototype.draw = function(ctx) {
   ctx.clear();
   this.atoms.draw(ctx);
@@ -38,9 +63,9 @@ function AtomList() {
 }
 
 AtomList.prototype.init = function(atoms) {
-  oatoms = Array();
+  var oatoms = Array();
   atoms.forEach(function(a) {
-    o = new Atom();
+    var o = new Atom();
     o.init(a.id, a.element, a.element_id, a.x, a.y);
     oatoms.push(o);
   });
@@ -52,7 +77,7 @@ AtomList.prototype.get = function(i) {
 }
 
 AtomList.prototype.propMin = function(p) {
-  min = this.atoms[0][p];
+  var min = this.atoms[0][p];
   this.atoms.forEach(function(a) {
     if(a[p] < min)
       min = a[p];
@@ -62,7 +87,7 @@ AtomList.prototype.propMin = function(p) {
 }
 
 AtomList.prototype.propMax = function(p) {
-  max = this.atoms[0][p];
+  var max = this.atoms[0][p];
   this.atoms.forEach(function(a) {
     if(a[p] > max)
       max = a[p];
@@ -72,8 +97,8 @@ AtomList.prototype.propMax = function(p) {
 }
 
 AtomList.prototype.propWidth = function(p) {
-  min = this.propMin(p);
-  max = this.propMax(p);
+  var min = this.propMin(p);
+  var max = this.propMax(p);
   return max - min;
 }
 
@@ -85,14 +110,6 @@ AtomList.prototype.height = function() {
   return this.propWidth('y');
 }
 
-AtomList.prototype.scale = function(f) {
-  this.atoms.map(function(a) {
-    a.x *= f;
-    a.y *= f;
-    return a;
-  });
-}
-
 AtomList.prototype.move = function(dx, dy) {
   this.atoms.map(function(a) {
     a.x += dx;
@@ -101,18 +118,24 @@ AtomList.prototype.move = function(dx, dy) {
   });
 }
 
+AtomList.prototype.scale = function(f) {
+  this.atoms.map(function(a) {
+    a.x *= f;
+    a.y *= f;
+    return a;
+  });
+}
+
 AtomList.prototype.bestFit = function(w, h) {
-  wf = w / this.width();
-  hf = h / this.height();
-  f = wf < hf ? wf : hf;
-  console.log(w, this.width(), wf, hf, f);
+  var wf = (w - CANVAS_PADDING) / this.width();
+  var hf = (h - CANVAS_PADDING) / this.height();
+  var f = wf < hf ? wf : hf;
   this.scale(f);
 
-  tx = w / 2 - this.width() / 2;
-  ty = h / 2 - this.height() / 2;
-  dx = tx - this.propMin('x');
-  dy = ty - this.propMin('y');
-  console.log(tx, ty, dx, dy);
+  var tx = w / 2 - this.width() / 2;
+  var ty = h / 2 - this.height() / 2;
+  var dx = tx - this.propMin('x');
+  var dy = ty - this.propMin('y');
   this.move(dx, dy);
 }
 
@@ -144,6 +167,14 @@ Atom.prototype.init = function(id, element, element_id, x, y, charge) {
   this.charge = charge;
 }
 
+Atom.prototype.dx = function(a) {
+  return a.x - this.x;
+}
+
+Atom.prototype.dy = function(a) {
+  return a.y - this.y;
+}
+
 Atom.prototype.draw = function(ctx) {
   ctx.fillText(this.element, this.x, this.y);
 }
@@ -157,10 +188,10 @@ function BondList() {
 }
 
 BondList.prototype.init = function(bonds, atoms) {
-  obonds = Array();
+  var obonds = Array();
   bonds.forEach(function(b) {
-    o = new Bond();
-    o.init(atoms.get(b.a1), atoms.get(b.a2), b.type);
+    var o = new Bond();
+    o.init(atoms.get(b.a1), atoms.get(b.a2), b.bond_type);
     obonds.push(o);
   });
   this.bonds = obonds;
@@ -189,6 +220,32 @@ Bond.prototype.init = function(a1, a2, type) {
 }
 
 Bond.prototype.draw = function(ctx) {
-  ctx.drawLine(this.a1.x, this.a1.y, this.a2.x, this.a2.y);
-}
+  // Leave some space around the atom
+  var dx = this.a1.dx(this.a2);
+  var dy = this.a1.dy(this.a2);
+  var dz = Math.sqrt(dx * dx + dy * dy);
+  
+  var ddx = dx * ATOM_RADIUS / dz;
+  var ddy = dy * ATOM_RADIUS / dz;
+  
+  var x1 = this.a1.x + ddx;
+  var y1 = this.a1.y + ddy;
+  var x2 = this.a2.x - ddx;
+  var y2 = this.a2.y - ddy;
+  
+  if(this.type == 1 || this.type == 3)
+    ctx.drawLine(x1, y1, x2, y2);
+  
+  if(this.type > 1) {
+    dx = x2 - x1;
+    dy = y2 - y1;
+    dz = Math.sqrt(dx * dx + dy * dy);
+    
+    ddx = dy * BOND_SPACING / dz;
+    ddy = dx * BOND_SPACING / dz;
 
+    // TODO: aromatic bonds
+    ctx.drawLine(x1 - ddx, y1 + ddy, x2 - ddx, y2 + ddy);
+    ctx.drawLine(x1 + ddx, y1 - ddy, x2 + ddx, y2 - ddy);
+  }
+}
