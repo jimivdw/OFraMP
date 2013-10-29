176c892db80a86e34bc3ffa87631c489898c66df
Jimi van der Woning <jimivdwoning@gmail.com>
2013-10-29 18:45:42 +0100

    Slightly refactored CanvasRenderingContext2D

--- src/static/js/extensions/CanvasRenderingContext2D.js
+++ src/static/js/extensions/CanvasRenderingContext2D.js
@@ -1,3 +1,5 @@
+DEFAULT_DASH_COUNT = 2;
+
 CanvasRenderingContext2D.prototype.centerPoint = function() {
   return {
     x: this.canvas.width / 2,
@@ -22,7 +24,7 @@ CanvasRenderingContext2D.prototype.drawDashedLine = function(x1, y1, x2, y2, n) 
   dy = y2 - y1;
   dz = Math.sqrt(dx * dx + dy * dy);
 
-  n = n || 2;
+  n = n || DEFAULT_DASH_COUNT;
   l = dz / (n * 2 - 1);
 
   ddx = dx * l / dz;
@@ -37,15 +39,15 @@ CanvasRenderingContext2D.prototype.drawDashedLine = function(x1, y1, x2, y2, n) 
 // http://www.html5canvastutorials.com/tutorials/html5-canvas-wrap-text-tutorial
 CanvasRenderingContext2D.prototype.textLines = function(x, y, w, h, text,
     hard_wrap) {
-  var ctx = this;
+  //var ctx = this;
 
   var lines = Array();
   var hard_lines = text.trim().split("\n");
-  hard_lines.forEach(function(hard_line) {
+  hard_lines.each(function(hard_line, ctx) {
     if(ctx.measureText(hard_line).width > w) {
       var line = "";
       var words = hard_line.split(" ");
-      words.forEach(function(word) {
+      words.each(function(word) {
         var nline = line + word + " ";
         if(ctx.measureText(nline).width > w) {
           lines.push(line);
@@ -53,7 +55,7 @@ CanvasRenderingContext2D.prototype.textLines = function(x, y, w, h, text,
           if(ctx.measureText(word).width > w && hard_wrap) {
             line = "";
             var chars = word.split("");
-            chars.forEach(function(char) {
+            chars.each(function(char) {
               var nline = line + char;
               if(ctx.measureText(nline).width > w) {
                 lines.push(line);
@@ -74,45 +76,40 @@ CanvasRenderingContext2D.prototype.textLines = function(x, y, w, h, text,
     } else {
       lines.push(hard_line);
     }
-  });
+  }, this);
   return lines;
 };
 
 CanvasRenderingContext2D.prototype.boxedFillText = function(x, y, w, h, text,
     hard_wrap) {
-  var ctx = this;
 
-  var lines = ctx.textLines(x, y, w, h, text, hard_wrap);
-  var lineHeight = parseInt(ctx.font.split("px")[0]) * 1.2;
+  var lines = this.textLines(x, y, w, h, text, hard_wrap);
+  var lineHeight = parseInt(this.font.split("px")[0]) * 1.2;
 
   var min_y = y;
   var max_y = y + h;
   var ll = lines.length - 1;
-  switch(ctx.textBaseline.toLowerCase()) {
-    case "bottom":
-      min_y = y - h + lineHeight;
-      max_y = y;
-      y -= ll * lineHeight;
-      if(y < min_y)
-        y = min_y;
-      break;
-
-    case "middle":
-      min_y = y - h / 2 + lineHeight / 2;
-      max_y = y + h / 2 - lineHeight / 2;
-      y -= (ll / 2) * lineHeight;
-      if(y < min_y)
-        y = min_y;
-      break;
-
-    default:
-      break;
+  var bl = this.textBaseline.toLowerCase();
+  if(bl === "bottom") {
+    min_y = y - h + lineHeight;
+    max_y = y;
+    y -= ll * lineHeight;
+    if(y < min_y) {
+      y = min_y;
+    }
+  } else if(bl === "middle") {
+    min_y = y - h / 2 + lineHeight / 2;
+    max_y = y + h / 2 - lineHeight / 2;
+    y -= (ll / 2) * lineHeight;
+    if(y < min_y) {
+      y = min_y;
+    }
   }
 
-  lines.forEach(function(line) {
+  lines.each(function(line, ctx) {
     if(y <= max_y) {
       ctx.fillText(line, x, y);
     }
     y += lineHeight;
-  });
+  }, this);
 };
