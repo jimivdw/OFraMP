6ed18f54ee04c73048fde8c5afb79f660d6a6117
Jimi van der Woning <jimivdwoning@gmail.com>
2013-10-30 12:01:47 +0100

    Refactored MoleculeViewer.

--- src/static/js/OFraMP/MoleculeViewer.js
+++ src/static/js/OFraMP/MoleculeViewer.js
@@ -1,212 +1,269 @@
 function MoleculeViewer(canvas_id, settings) {
-  this.molecule = undefined;
-  this.canvas = document.getElementById(canvas_id);
-  this.ctx = this.canvas.getContext("2d");
-  this.init_context();
-
-  this.overlay_showing = false;
-  this.overlay_msg = "";
-  this.overlay_status = 1;
-
-  this.settings = DEFAULT_SETTINGS.copy().merge(settings);
-
-  if(this.settings.interactive) {
-    this.init_interaction();
-  }
+  this.init(canvas_id, settings);
 }
 
-MoleculeViewer.prototype.init_context = function() {
-  this.ctx.textAlign = 'center';
-  this.ctx.textBaseline = 'middle';
-};
+MoleculeViewer.prototype = {
+  molecule: undefined,
+  canvas: undefined,
+  ctx: undefined,
 
-MoleculeViewer.prototype.init_interaction = function() {
-  var mv = this;
+  overlay_showing: false,
+  overlay_message: "",
+  overlay_status: 1,
 
-  // TODO: Not supported in Firefox!
-  this.canvas.onmousewheel = function(e) {
-    if(!mv.overlay_showing) {
-      if(e.wheelDelta > 0) {
-        var f = mv.settings.zoom_factor;
-      } else {
-        var f = 1 / mv.settings.zoom_factor;
-      }
-      mv.zoomOn(e.offsetX, e.offsetY, f);
+  settings: undefined,
 
-      return false;
+  init: function(canvas_id, settings) {
+    this.canvas = document.getElementById(canvas_id);
+    this.init_context();
+
+    this.settings = DEFAULT_SETTINGS.copy().merge(settings);
+
+    if(this.settings.interactive) {
+      this.init_interaction();
     }
-  };
-
-  this.canvas.onmousedown = function(e) {
-    if(!mv.overlay_showing) {
-      var a = mv.molecule.atomAt(e.getX(), e.getY());
-      if(a) {
-        if(mv.molecule.setSelected(a))
-          mv.redraw();
-      } else {
-        mv.lastX = e.getX();
-        mv.lastY = e.getY();
-        mv.mouseDown = true;
+  },
+  
+  init_context: function() {
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.textAlign = 'center';
+    this.ctx.textBaseline = 'middle';
+  },
+  
+  init_interaction: function() {
+    var mv = this;
+  
+    // TODO: Not supported in Firefox!
+    this.canvas.onmousewheel = function(e) {
+      if(!mv.overlay_showing) {
+        if(e.wheelDelta > 0) {
+          var f = mv.settings.zoom_factor;
+        } else {
+          var f = 1 / mv.settings.zoom_factor;
+        }
+        mv.zoomOn(e.offsetX, e.offsetY, f);
+  
+        return false;
       }
-    }
-  };
-
-  this.canvas.onmousemove = function(e) {
-    if(!mv.overlay_showing) {
-      if(mv.mouseDown) {
-        var dx = e.getX() - mv.lastX;
-        var dy = e.getY() - mv.lastY;
-        mv.move(dx, dy);
-
-        mv.lastX = e.getX();
-        mv.lastY = e.getY();
-        mv.mouseDragged = true;
-      } else {
+    };
+  
+    this.canvas.onmousedown = function(e) {
+      if(!mv.overlay_showing) {
         var a = mv.molecule.atomAt(e.getX(), e.getY());
-        if(mv.molecule.setHover(a))
-          mv.redraw();
+        if(a) {
+          if(mv.molecule.setSelected(a))
+            mv.redraw();
+        } else {
+          mv.lastX = e.getX();
+          mv.lastY = e.getY();
+          mv.mouseDown = true;
+        }
       }
-    }
-  };
-
-  document.onmouseup = function(e) {
-    if(!mv.overlay_showing) {
-      if(e.target === mv.canvas && !mv.mouseDragged) {
-        if(!mv.molecule.atomAt(e.getX(), e.getY())) {
-          if(mv.molecule.setSelected())
+    };
+  
+    this.canvas.onmousemove = function(e) {
+      if(!mv.overlay_showing) {
+        if(mv.mouseDown) {
+          var dx = e.getX() - mv.lastX;
+          var dy = e.getY() - mv.lastY;
+          mv.move(dx, dy);
+  
+          mv.lastX = e.getX();
+          mv.lastY = e.getY();
+          mv.mouseDragged = true;
+        } else {
+          var a = mv.molecule.atomAt(e.getX(), e.getY());
+          if(mv.molecule.setHover(a))
             mv.redraw();
         }
       }
-      mv.mouseDown = false;
-      mv.mouseDragged = false;
-    }
-  };
-};
-
-MoleculeViewer.prototype.showMolecule = function(data_str) {
-  var mv = this;
-
-  this.showOverlay("Loading molecule data...", MESSAGE_TYPES.info);
-
-  var xhr = new XMLHttpRequest();
-
-  xhr.onreadystatechange = function() {
-    if(xhr.readyState == 1) {
-      mv.showOverlay("Loading molecule data...\nConnection established.");
-    } else if(xhr.readyState == 2) {
-      mv.showOverlay("Loading molecule data...\nRequest received.");
-    } else if(xhr.readyState == 3) {
-      mv.showOverlay("Loading molecule data...\nProcessing request...");
-    } else if(xhr.readyState == 4 && xhr.status == 200) {
-      var md = JSON.parse(xhr.response);
-      console.log("md", md);
-
-      if(md.error) {
-        mv.showOverlay(md.error, MESSAGE_TYPES.error);
-      } else if(md.atoms && md.bonds) {
-        mv.showOverlay("Initializing molecule...");
-        mv.molecule = new Molecule(mv, md.atoms, md.bonds);
-        mv.hideOverlay();
-
-        mv.idealize();// bestFit();
-      } else {
-        mv.showOverlay("Missing data, received: " + md.show(),
-            MESSAGE_TYPES.critical);
+    };
+  
+    document.onmouseup = function(e) {
+      if(!mv.overlay_showing) {
+        if(e.target === mv.canvas && !mv.mouseDragged) {
+          if(!mv.molecule.atomAt(e.getX(), e.getY())) {
+            if(mv.molecule.setSelected())
+              mv.redraw();
+          }
+        }
+        mv.mouseDown = false;
+        mv.mouseDragged = false;
       }
-    } else if(xhr.status != 200) {
-      mv.showOverlay("Could not connect to server", MESSAGE_TYPES.critical);
+    };
+  },
+  
+  /*
+   * Load and show a molecule, represented by data_str (currently SMILES).
+   */
+  showMolecule: function(data_str) {
+    var mv = this;
+  
+    this.showOverlay("Loading molecule data...", MESSAGE_TYPES.info);
+  
+    var xhr = new XMLHttpRequest();
+  
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState == 1) {
+        mv.showOverlay("Loading molecule data...\nConnection established.");
+      } else if(xhr.readyState == 2) {
+        mv.showOverlay("Loading molecule data...\nRequest received.");
+      } else if(xhr.readyState == 3) {
+        mv.showOverlay("Loading molecule data...\nProcessing request...");
+      } else if(xhr.readyState == 4 && xhr.status == 200) {
+        var md = JSON.parse(xhr.response);
+        console.log("md", md);
+  
+        if(md.error) {
+          mv.showOverlay(md.error, MESSAGE_TYPES.error);
+        } else if(md.atoms && md.bonds) {
+          mv.showOverlay("Initializing molecule...");
+          mv.molecule = new Molecule(mv, md.atoms, md.bonds);
+          mv.hideOverlay();
+  
+          mv.idealize();
+        } else {
+          mv.showOverlay("Missing data, received: " + md.show(),
+              MESSAGE_TYPES.critical);
+        }
+      } else if(xhr.status != 200) {
+        mv.showOverlay("Could not connect to server", MESSAGE_TYPES.critical);
+      }
+    };
+  
+    xhr.open("POST", this.settings.oapoc_url, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send("fmt=smiles&data=" + data_str);
+  },
+  
+  /*
+   * Show an overlay with a given message msg of status status.
+   * 
+   * Both msg and status will be retrieved from 'this' if they are undefined.
+   */
+  showOverlay: function(msg, status) {
+    if(this.overlay_showing) {
+      this.hideOverlay();
     }
-  };
-
-  xhr.open("POST", this.settings.oapoc_url, true);
-  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.send("fmt=smiles&data=" + data_str);
-};
-
-MoleculeViewer.prototype.showOverlay = function(msg, status) {
-  if(this.overlay_showing) {
-    this.hideOverlay();
+  
+    msg = msg || this.overlay_msg;
+    status = status || this.overlay_status;
+  
+    var ctx = this.ctx;
+    ctx.fillStyle = this.settings.message_border_color;
+    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  
+    ctx.fillStyle = this.settings.message_bg_colors[status];
+    var bw = this.settings.message_border_width;
+    ctx.fillRect(bw, bw, this.canvas.width - 2 * bw, this.canvas.height - 2 * bw);
+  
+    ctx.font = this.settings.message_font;
+    ctx.fillStyle = this.settings.message_color;
+    var p = this.settings.message_padding + bw;
+    ctx.boxedFillText(this.canvas.width / 2, this.canvas.height / 2,
+        this.canvas.width - 2 * p, this.canvas.height - 2 * p, msg, true);
+  
+    this.canvas.style.cursor = this.settings.canvas_cursor_normal;
+  
+    this.overlay_showing = true;
+    this.overlay_msg = msg;
+    this.overlay_status = status;
+  },
+  
+  /*
+   * Hide the overlay.
+   */
+  hideOverlay: function() {
+    this.overlay_showing = false;
+    this.redraw();
+  },
+  
+  /*
+   * Redraw the canvas.
+   */
+  redraw: function() {
+    this.ctx.clear();
+    
+    if(this.molecule) {
+      this.molecule.draw();
+    }
+    
+    if(this.overlay_showing) {
+      this.showOverlay();
+    }
+  },
+  
+  /*
+   * Move the molecule dx in the x direction and dy on the y axis.
+   */
+  move: function(dx, dy) {
+    if(!this.settings.interactive || !this.molecule) {
+      return;
+    }
+  
+    this.molecule.move(dx, dy);
+    this.redraw();
+  },
+  
+  /*
+   * Zoom on the center of the molecule with a factor f.
+   */
+  zoom: function(f) {
+    if(!this.settings.interactive || !this.molecule) {
+      return;
+    }
+  
+    // Restrict minimum and maximum zoom
+    var ad = this.molecule.bonds.averageDistance();
+    if(f < 1 && ad < this.settings.min_zoom || f > 1
+        && ad > this.settings.max_zoom) {
+      return;
+    }
+  
+    this.molecule.zoom(f);
+    this.redraw();
+  },
+  
+  /*
+   * Zoom on the position (x, y) with a factor f.
+   */
+  zoomOn: function(x, y, f) {
+    if(!this.settings.interactive || !this.molecule) {
+      return;
+    }
+  
+    // Restrict minimum and maximum zoom
+    var ad = this.molecule.bonds.averageDistance()
+    if(f < 1 && ad < this.settings.min_zoom || f > 1
+        && ad > this.settings.max_zoom) {
+      return;
+    }
+  
+    this.molecule.zoomOn(x, y, f);
+    this.redraw();
+  },
+  
+  /*
+   * Fit the molecule on the canvas.
+   */
+  bestFit: function() {
+    if(!this.molecule) {
+      return;
+    }
+    
+    this.molecule.bestFit(this.canvas.width, this.canvas.height);
+    this.redraw();
+  },
+  
+  /*
+   * Show the molecule in ideal size on the canvas.
+   */
+  idealize: function() {
+    if(!this.molecule) {
+      return;
+    }
+    
+    this.molecule.idealize();
+    this.redraw();
   }
-
-  msg = msg || this.overlay_msg;
-  status = status || this.overlay_status;
-
-  var ctx = this.ctx;
-  ctx.fillStyle = this.settings.message_border_color;
-  ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
-  ctx.fillStyle = this.settings.message_bg_colors[status];
-  var bw = this.settings.message_border_width;
-  ctx.fillRect(bw, bw, this.canvas.width - 2 * bw, this.canvas.height - 2 * bw);
-
-  ctx.font = this.settings.message_font;
-  ctx.fillStyle = this.settings.message_color;
-  var p = this.settings.message_padding + bw;
-  ctx.boxedFillText(this.canvas.width / 2, this.canvas.height / 2,
-      this.canvas.width - 2 * p, this.canvas.height - 2 * p, msg, true);
-
-  this.canvas.style.cursor = this.settings.canvas_cursor_normal;
-
-  this.overlay_showing = true;
-  this.overlay_msg = msg;
-  this.overlay_status = status;
 };
-
-MoleculeViewer.prototype.hideOverlay = function() {
-  this.overlay_showing = false;
-  this.redraw();
-};
-
-MoleculeViewer.prototype.redraw = function() {
-  this.ctx.clear();
-  if(this.molecule)
-    this.molecule.draw();
-  if(this.overlay_showing)
-    this.showOverlay();
-};
-
-MoleculeViewer.prototype.move = function(dx, dy) {
-  if(!this.settings.interactive)
-    return;
-
-  this.molecule.move(dx, dy);
-  this.redraw();
-};
-
-MoleculeViewer.prototype.zoomOn = function(x, y, f) {
-  if(!this.settings.interactive)
-    return;
-
-  // Restrict minimum and maximum zoom
-  var ad = this.molecule.bonds.averageDistance()
-  if(f < 1 && ad < this.settings.min_zoom || f > 1
-      && ad > this.settings.max_zoom)
-    return;
-
-  this.molecule.zoomOn(x, y, f);
-  this.redraw();
-};
-
-MoleculeViewer.prototype.zoom = function(f) {
-  if(!this.settings.interactive)
-    return;
-
-  // Restrict minimum and maximum zoom
-  var ad = this.molecule.bonds.averageDistance()
-  if(f < 1 && ad < this.settings.min_zoom || f > 1
-      && ad > this.settings.max_zoom)
-    return;
-
-  this.molecule.zoom(f);
-  this.redraw();
-};
-
-MoleculeViewer.prototype.bestFit = function() {
-  this.molecule.bestFit(this.canvas.width, this.canvas.height);
-  this.redraw();
-};
-
-MoleculeViewer.prototype.idealize = function() {
-  this.molecule.idealize();
-  this.redraw();
-}
