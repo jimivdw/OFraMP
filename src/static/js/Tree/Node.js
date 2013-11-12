b666e84fd25796a6d0b77ab21988f7b0c2d94352
Jimi van der Woning <jimivdwoning@gmail.com>
2013-11-12 13:39:28 +0100

    Added depth, BFS find, and path finding methods to trees.

--- src/static/js/Tree/Node.js
+++ src/static/js/Tree/Node.js
@@ -14,6 +14,17 @@ Node.prototype = {
     this.children = new Array();
   },
 
+  depth: function() {
+    depths = $ext.array.map(this.children, function(child) {
+      return child.depth();
+    }, this);
+    if(depths.length > 0) {
+      return 1 + $ext.array.max(depths);
+    } else {
+      return 1;
+    }
+  },
+
   addChild: function(key, value) {
     if(this.find(key)) {
       return false;
@@ -37,8 +48,8 @@ Node.prototype = {
     }
   },
 
-  findNode: function(key) {
-    if(this.key === key) {
+  findNode: function(key, skip) {
+    if(this.key === key && !skip) {
       return this;
     }
 
@@ -50,11 +61,76 @@ Node.prototype = {
     });
   },
 
+  /*
+   * Breadth first serch for the node with the given key.
+   * 
+   * Skip the first result if skip is true (useful for finding cycles).
+   */
+  findNodeB: function(key, skip) {
+    var q = [this];
+    var pq = [[]];
+    while(q.length > 0) {
+      var n = q.shift();
+      var p = pq.shift();
+      if(n.key === key && (!skip || p.length > 0)) {
+        return n;
+      }
+
+      $ext.each(n.children, function(child) {
+        if(p.indexOf(child.key) === -1) {
+          q.push(child);
+          pq.push(p.concat(child.key));
+        }
+      });
+    }
+  },
+
   find: function(key) {
     var n = this.findNode(key);
     if(n) {
       return n.value;
     }
+  },
+
+  /*
+   * Breadth first search for the value of the node with the given key.
+   */
+  findB: function(key) {
+    var n = this.findNodeB(key);
+    if(n) {
+      return n.value;
+    }
+  },
+
+  findPath: function(key) {
+    var n = this.findNode(key, true);
+    if(!n) {
+      return [];
+    }
+
+    var p = new Array();
+    while(n.parent) {
+      p.push(n.value);
+      n = n.parent;
+    }
+    return p;
+  },
+
+  /*
+   * Find the shortest path from this node to the node with the given key.
+   */
+  findShortestPath: function(key) {
+    var n = this.findNodeB(key, true);
+    if(!n) {
+      return [];
+    }
+
+    var p = new Array();
+    while(n.parent) {
+      p.push(n.value);
+      n = n.parent;
+    }
+    return p;
   },
 
   flatten: function(tgt) {
