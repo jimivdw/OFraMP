17b6ac883137c160e2015bf9750ed55699fa7732
Jimi van der Woning <jimivdwoning@gmail.com>
2013-11-27 15:32:10 +0100

    Fix for IE7's lack of Elements.

--- src/static/js/compatibility/Element.js
+++ src/static/js/compatibility/Element.js
@@ -1,3 +1,32 @@
+// From: http://stackoverflow.com/questions/597268/element-prototype-in-ie7
+(function() {
+  if(!window.Element) {
+    Element = function() {};
+
+    var __createElement = document.createElement;
+    document.createElement = function(tagName) {
+      var element = __createElement(tagName);
+      if(element == null) {
+        return null;
+      }
+      for( var key in Element.prototype)
+        element[key] = Element.prototype[key];
+      return element;
+    }
+
+    var __getElementById = document.getElementById;
+    document.getElementById = function(id) {
+      var element = __getElementById(id);
+      if(element == null) {
+        return null;
+      }
+      for( var key in Element.prototype)
+        element[key] = Element.prototype[key];
+      return element;
+    }
+  }
+})();
+
 (function() {
   if(document.createElement("div").firstChild === undefined) {
     Object.defineProperty(Element.prototype, 'firstChild', {
