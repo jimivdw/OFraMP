2dabb8c40af8740f71b30796946e373af41f2ef3
Jimi van der Woning <jimivdwoning@gmail.com>
2013-12-18 14:01:10 +0100

    Minor styling fixes.

--- src/static/js/OFraMP/settings.js
+++ src/static/js/OFraMP/settings.js
@@ -343,10 +343,3 @@ var SETTINGS_OPTIONS = {
     }
   }
 };
-
-
-/*
- * Nice colors: Gray: rgb(148, 148, 148) Green: rgb( 80, 169, 75) Blue: rgb( 76,
- * 81, 178) Red: rgb(203, 83, 73) Yellow: rgb(204, 166, 40) Brown: rgb(127, 79,
- * 66) Purple: rgb(227, 119, 219)
- */
