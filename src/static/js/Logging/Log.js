ffa19f85c0c00fd376cc69897331423f047b7f67
Jimi van der Woning <jimivdwoning@gmail.com>
2014-01-20 14:18:11 +0100

    Better logs.

--- src/static/js/Logging/Log.js
+++ src/static/js/Logging/Log.js
@@ -1,11 +1,38 @@
-var __log = new Array();
+var __log = new Object();
 
 function log(type, desc) {
   var now = Date.now();
-  type = type || "generic";
-  desc = desc || "";
-  var msg = "" + now + ";" + type + ";" + desc + ";";
-  __log.push(msg);
+  type = type || "other";
+  desc = desc || type;
+
+  var tp = type.split(".");
+  var cl = __log;
+  $ext.each(tp, function(subtype, i) {
+    if(cl[subtype] === undefined) {
+      if(i === tp.length - 1) {
+        cl[subtype] = new Array();
+      } else {
+        cl[subtype] = new Object();
+      }
+    }
+    cl = cl[subtype];
+  });
+
+  var lo = {
+    time: now,
+    msg: desc
+  };
+  cl.push(lo);
 }
 
-log("logging.create", "Logger created");
+function getCSVLog() {
+  var csvl = new Array();
+  $ext.each(__log, function(v, type) {
+    $ext.each(v, function(entry) {
+      csvl.push("" + entry.time + ";" + type + ";" + entry.msg + ";");
+    });
+  }, undefined, true);
+  return csvl.sort();
+}
+
+log("system.init.logger", "Logger created");
