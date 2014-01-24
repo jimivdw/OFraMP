b1eeae53bf4d9b1bf79fedbe4438aefa04a78e22
Jimi van der Woning <jimivdwoning@gmail.com>
2014-01-24 17:36:38 +0100

    Now also updated OAPoC version string.

--- src/static/js/OFraMP/settings.js
+++ src/static/js/OFraMP/settings.js
@@ -20,7 +20,7 @@ var DEFAULT_SETTINGS = {
   oapoc: {
     url: "http://vps955.directvps.nl/OAPoC/generate/",
     load_url: "http://vps955.directvps.nl/OAPoC/loadATB/",
-    version: "0.3.0"
+    version: "0.4.0"
   },
 
   omfraf: {
