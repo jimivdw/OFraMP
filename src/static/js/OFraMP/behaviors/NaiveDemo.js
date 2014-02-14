f79efeb1ff9968e060eefadcf5858dd1ed860c7f
Jimi van der Woning <jimivdwoning@gmail.com>
2014-02-14 14:34:51 +0100

    Demo molecule is now slightly bigger to ensure the need for fragment comparison.

--- src/static/js/OFraMP/behaviors/NaiveDemo.js
+++ src/static/js/OFraMP/behaviors/NaiveDemo.js
@@ -21,7 +21,7 @@ NaiveDemo.prototype = {
     $ext.dom.addClass(mi, "highlighted");
     var cbs = $ext.dom.onMouseClick(mi, function() {
       mi.disabled = "disabled";
-      $ext.dom.addText(mi, "NCC(=O)C");
+      $ext.dom.addText(mi, "NCC(=O)CCO");
       $ext.dom.removeClass(mi, "highlighted");
       $ext.dom.removeEventListeners(mi, cbs);
       _this.nextStep();
