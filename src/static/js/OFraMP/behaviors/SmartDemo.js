b1835d93dac3e548bd44483b7b29154adf905f4f
Jimi van der Woning <jimivdwoning@gmail.com>
2014-02-16 17:53:30 +0100

    Finished Smart demo

--- src/static/js/OFraMP/behaviors/SmartDemo.js
+++ src/static/js/OFraMP/behaviors/SmartDemo.js
@@ -101,8 +101,9 @@ SmartDemo.prototype = {
     this.overlay.style.paddingLeft = "50px";
     this.overlay.style.background = "url('static/img/demo/arrow_down_2.png') bottom left no-repeat";
 
-    $ext.dom.addText(this.overlay, "In order to properly judge a fragment, its "
-        + "original molecule can be shown using the 'View original' button");
+    $ext.dom.addText(this.overlay, "In order to properly judge a fragment, "
+        + "its original molecule can be shown using the 'View original' "
+        + "button");
 
     var omb = document.getElementById("view_original");
     var afb = document.getElementById("accept_fragment");
@@ -112,10 +113,133 @@ SmartDemo.prototype = {
     rfb.disabled = "disabled";
     var cbs = $ext.dom.onMouseClick(omb, function() {
       $ext.dom.removeClass(omb, "highlighted");
+      omb.disabled = "disabled";
       $ext.dom.removeEventListeners(omb, cbs);
       _this.nextStep();
     }, $ext.mouse.LEFT);
-  }
+  },
+
+  step9: NaiveDemo.prototype.step10,
+
+  step10: function() {
+    var _this = this;
+
+    $ext.dom.clear(this.overlay);
+    this.overlay.style.cssText = "";
+    this.overlay.style.bottom = "50px";
+    this.overlay.style.left = "50%";
+    this.overlay.style.width = "320px";
+    this.overlay.style.marginLeft = "-320px";
+    this.overlay.style.paddingBottom = "15px";
+    this.overlay.style.paddingRight = "50px";
+    this.overlay.style.background = "url('static/img/demo/arrow_down.png') bottom right no-repeat";
+
+    $ext.dom.addText(this.overlay, "This fragment's charges can be chosen by "
+        + "clicking the 'Accept' button");
+
+    var afb = document.getElementById("accept_fragment");
+    $ext.dom.addClass(afb, "highlighted");
+    afb.disabled = "";
+    var cbs = $ext.dom.onMouseClick(afb, function() {
+      $ext.dom.removeClass(afb, "highlighted");
+      afb.disabled = "disabled";
+      $ext.dom.removeEventListeners(afb, cbs);
+      _this.nextStep();
+    }, $ext.mouse.LEFT);
+  },
+
+  step11: function() {
+    var _this = this;
+
+    if(this.oframp.behavior.__fragments !== undefined) {
+      return this.nextStep();
+    }
+
+    $ext.dom.clear(this.overlay);
+    this.overlay.style.cssText = "";
+
+    this.overlay.style.bottom = "50px";
+    this.overlay.style.left = "50%";
+    this.overlay.style.width = "540px";
+    this.overlay.style.marginLeft = "-270px";
+
+    $ext.dom.addText(this.overlay, "The system is looking for fragments, "
+        + "please wait");
+
+    $ext.dom.addEventListener(this.oframp.container, "fragmentsfound",
+        fragmentsFound);
+    function fragmentsFound() {
+      $ext.dom.removeEventListener(_this.oframp.container, "fragmentsfound",
+          fragmentsFound);
+      _this.nextStep();
+    }
+  },
+
+  step12: function() {
+    var _this = this;
+
+    $ext.dom.clear(this.overlay);
+    this.overlay.style.cssText = "";
+    this.overlay.style.bottom = "50px";
+    this.overlay.style.left = "50%";
+    this.overlay.style.width = "380px";
+    this.overlay.style.marginLeft = "-230px";
+    this.overlay.style.paddingBottom = "15px";
+    this.overlay.style.paddingRight = "50px";
+    this.overlay.style.background = "url('static/img/demo/arrow_down.png') bottom right no-repeat";
+
+    $ext.dom.addText(this.overlay, "When a fragment is not considered a good "
+        + "match, it can be discarded using the 'Reject' button");
+
+    var rfb = document.getElementById("reject_fragment");
+    $ext.dom.addClass(rfb, "highlighted");
+    rfb.disabled = "";
+    var cbs = $ext.dom.onMouseClick(rfb, function() {
+      $ext.dom.removeClass(rfb, "highlighted");
+      rfb.disabled = "disabled";
+      $ext.dom.removeEventListeners(rfb, cbs);
+      _this.nextStep();
+    }, $ext.mouse.LEFT);
+  },
+
+  step13: function() {
+    var _this = this;
+
+    $ext.dom.clear(this.overlay);
+    this.overlay.style.cssText = "";
+
+    this.overlay.style.top = "40px";
+    this.overlay.style.left = "50%";
+    this.overlay.style.width = "380px";
+    this.overlay.style.marginLeft = "-310px";
+    this.overlay.style.paddingTop = "15px";
+    this.overlay.style.paddingRight = "50px";
+    this.overlay.style.background = "url('static/img/demo/arrow_up.png') top right no-repeat";
+
+    $ext.dom.addText(this.overlay, "When a fragment has accidentally been "
+        + "rejected, you can undo this using the 'Undo' button");
+
+    var omb = document.getElementById("view_original");
+    var afb = document.getElementById("accept_fragment");
+    var rfb = document.getElementById("reject_fragment");
+
+    var ub = document.getElementById("undo");
+    $ext.dom.addClass(ub, "highlighted");
+    var cbs = $ext.dom.onMouseClick(ub, function() {
+      $ext.dom.removeClass(ub, "highlighted");
+      $ext.dom.removeEventListeners(ub, cbs);
+
+      omb.disabled = "";
+      afb.disabled = "";
+      rfb.disabled = "";
+
+      _this.nextStep();
+    }, $ext.mouse.LEFT);
+  },
+
+  step14: NaiveDemo.prototype.step14,
+
+  step15: NaiveDemo.prototype.step15
 };
 
 SmartDemo.prototype = $ext.extend($ext.copy(Demo.prototype),
