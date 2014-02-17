4f4c2bcdb7e5801f8b11a30068fbdef1595b654e
Jimi van der Woning <jimivdwoning@gmail.com>
2014-02-17 12:01:44 +0100

    Added simple cookie library.

---
+++ src/static/js/extensions/Cookie.js
@@ -0,0 +1,36 @@
+var cookie = {
+  getAll: function() {
+    var co = {};
+    $ext.each(document.cookie.split(';'), function(cp) {
+      cp = cp.trim();
+      var kv = cp.split('=');
+      co[kv[0]] = kv[1];
+    });
+    return co;
+  },
+
+  get: function(name) {
+    return this.getAll()[name];
+  },
+
+  set: function(name, value, expireDays) {
+    if(expireDays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (expireDays * 24 * 60 * 60 * 1000));
+      var expires = "; expires=" + d.toGMTString();
+    } else {
+      var expires = "";
+    }
+    document.cookie = name + "=" + value + expires;
+  },
+
+  delete: function(name) {
+    name += "=";
+    var d = new Date('1970-01-01');
+    document.cookie = name + "; expires=" + d.toGMTString();
+  }
+};
+
+$ext.extend($ext, {
+  cookie: cookie
+});
