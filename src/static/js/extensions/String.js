db01905bcb394ee4cb231d33f23289db4f8c5e09
Jimi van der Woning <jimivdwoning@gmail.com>
2014-02-24 14:10:44 +0100

    Fixed formatting of String.js.

--- src/static/js/extensions/String.js
+++ src/static/js/extensions/String.js
@@ -1,55 +1,57 @@
-$ext.extend($ext, {
-  string: {
-    /*
-     * Repeat a string n times.
-     */
-    repeat: function(str, n) {
-      var r = "";
-      for( var i = 0; i < n; i++) {
-        r += str;
-      }
-      return r;
-    },
+var string = {
+  /*
+   * Repeat a string n times.
+   */
+  repeat: function(str, n) {
+    var r = "";
+    for( var i = 0; i < n; i++) {
+      r += str;
+    }
+    return r;
+  },
 
-    /*
-     * Reverse slice, returns the opposite of slice.
-     */
-    rslice: function(str, i) {
-      if(!str) {
-        return "";
-      }
+  /*
+   * Reverse slice, returns the opposite of slice.
+   */
+  rslice: function(str, i) {
+    if(!str) {
+      return "";
+    }
 
-      var chars = str.split("");
-      chars.splice(i, chars.length - i);
-      return chars.join("");
-    },
+    var chars = str.split("");
+    chars.splice(i, chars.length - i);
+    return chars.join("");
+  },
 
-    /*
-     * Compare two version strings, e.g. "1.9.4" or "2.0".
-     * 
-     * Return -1 if other is older, 0 if equal and 1 if newer.
-     */
-    versionCompare: function(str, other) {
-      if(!str || !other) {
+  /*
+   * Compare two version strings, e.g. "1.9.4" or "2.0".
+   * 
+   * Return -1 if other is older, 0 if equal and 1 if newer.
+   */
+  versionCompare: function(str, other) {
+    if(!str || !other) {
+      return -1;
+    }
+
+    var tparts = str.split(".");
+    var oparts = other.split(".");
+    var i;
+    for(i = 0; i < tparts.length; i++) {
+      if(!oparts[i] || tparts[i] > oparts[i]) {
         return -1;
-      }
-
-      var tparts = str.split(".");
-      var oparts = other.split(".");
-      var i;
-      for(i = 0; i < tparts.length; i++) {
-        if(!oparts[i] || tparts[i] > oparts[i]) {
-          return -1;
-        } else if(tparts[i] < oparts[i]) {
-          return 1;
-        }
-      }
-
-      if(oparts[i] > 0) {
+      } else if(tparts[i] < oparts[i]) {
         return 1;
-      } else {
-        return 0;
       }
     }
+
+    if(oparts[i] > 0) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
+};
+
+$ext.extend($ext, {
+  string: string
 });
