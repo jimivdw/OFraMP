34f1f594e31b3fbe2031dbf10344bcb067c440f1
Jimi van der Woning <jimivdwoning@gmail.com>
2014-02-24 12:00:56 +0100

    Repository button is now also disabled in Demo.

--- src/static/js/OFraMP/Demo.js
+++ src/static/js/OFraMP/Demo.js
@@ -55,8 +55,8 @@ Demo.prototype = {
   step0: function() {
     this.oframp.showInsertMoleculePopup();
     // TODO: random_molecule will be removed
-    var td = ["shell_size", "load_oss", "random_molecule", "mds_submit", "new",
-        "open", "snap", "save", "extra_controls_toggle"];
+    var td = ["repository", "shell_size", "load_oss", "random_molecule",
+        "mds_submit", "new", "open", "snap", "save", "extra_controls_toggle"];
     $ext.each(td, function(id) {
       var elem = document.getElementById(id);
       if(elem) {
